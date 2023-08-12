a2301694dc99aa6a7b5011f45cc444e23580185f
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2023-08-12 17:56:42 +0530

    creted react app

---
+++ first-app/src/App.js
@@ -0,0 +1,19 @@
+function Image(){
+  return (
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJzMNWNPsA8KiUFY_YiEC7rub3JEDOCUXXHwJ40dp7&s" alt="" />
+  )
+}
+function App() {
+  return (
+    <div>
+      hello world
+      <Image />
+      <Image />
+
+      <Image />
+
+    </div>
+     );
+}
+
+export default App;
